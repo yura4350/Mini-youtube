836db035df95b9cd8998d0fe2d069288d3c050c6
Iurii Beliaev <ib86@duke.edu>
2026-03-28 05:18:18 -0400

    Feat USER_ACCOUNTS_PREFERENCES-01: add JWT logic and password hashing with a dummy database

--- src/user_prefs_and_accounts_service/main.py
+++ src/user_prefs_and_accounts_service/main.py
@@ -9,7 +9,7 @@ SECRET_KEY = "fc42e6a3e2c0cd478aaea480e41fdcc1d4bb802c7d41fccbff59465f6945f190"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-fake_db = {
+db = {
     "iurii": {
         "username": "iurii",
         "email": "iurii@gmail.com",
@@ -77,6 +77,37 @@ async def get_current_user(token: str = Depends(oath_2_scheme)):
             raise credential_exception
         
         token_data = TokenData(username=username)
-        
+
     except JWTError:
         raise credential_exception
+
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credential_exception
+    
+    return user
+
+async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+
+    return current_user
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password", headers={"WWW-Authenticate":"Bearer"})
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+@app.get("/users/me/itesms")
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": 1, "owner": current_user}]
