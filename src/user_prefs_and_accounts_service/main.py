e6eb75acb07a7eeee7e77f00ed7a679df0d0c593
Iurii Beliaev <ib86@duke.edu>
2026-04-05 01:38:38 -0400

    [USER_ACCOUNTS_PREFERENCES-02A] Feature: add more appropriate error handling

--- src/user_prefs_and_accounts_service/main.py
+++ src/user_prefs_and_accounts_service/main.py
@@ -178,19 +178,21 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
     return db_user
 
 @app.post("/token", response_model=Token)
-def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session=Depends(get_db)):
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session=Depends(get_db)):
     user = db.query(User).filter(User.email == form_data.username).first()
 
     if not user or not verify_pwd(form_data.password, user.hashed_pwd):
         raise HTTPException(
-            status_code=404,
-            detail="Wrong info!"
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Bearer"},
         )
     
     if not user.is_active:
         raise HTTPException(
-            status_code=404,
-            detail="Inactive User!"
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Bearer"},
         )
     
     access_token_expires = timedelta(minutes=TOKEN_EXPIRES)
