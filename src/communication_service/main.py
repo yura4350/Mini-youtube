3fe7d15c07801936c1cae6f8b448f9826ddeda40
Iurii Beliaev <ib86@duke.edu>
2026-04-15 15:15:51 -0400

    Debug deployment: add my VM into the allowed list of CORS origins for remaining tables

--- src/communication_service/main.py
+++ src/communication_service/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://vcm-52418.vm.duke.edu:5173",
+        "http://vcm-52527.vm.duke.edu:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
