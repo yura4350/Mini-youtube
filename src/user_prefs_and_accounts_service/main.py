64acb3fba244efc9dec734583d4a5fd4aaad3600
Iurii Beliaev <ib86@duke.edu>
2026-04-14 23:55:30 -0400

    Feature USER_ACCOUNTS_PREFERENCES-08: implement endpoint to update the user preferences

--- src/user_prefs_and_accounts_service/main.py
+++ src/user_prefs_and_accounts_service/main.py
@@ -393,6 +393,21 @@ def get_user_preferences(current_user:User = Depends(get_current_active_user), d
     user_prefs = _get_or_create_user_preferences(db, current_user.id)
     return user_prefs
 
+# Update user's own preferences
+@app.patch("/user/preferences/update", response_model=UserPreferencesResponse)
+def update_user_preferences(update_prefs:UserPreferencesUpdate, current_user:User = Depends(get_current_active_user), db:Session = Depends(get_db)):
+    user_prefs = _get_or_create_user_preferences(db, current_user.id)
+    if update_prefs.privacy is not None:
+        user_prefs.privacy = update_prefs.privacy
+    if update_prefs.notifications is not None:
+        user_prefs.notifications = update_prefs.notifications
+    if update_prefs.ui_theme is not None:
+        user_prefs.ui_theme = update_prefs.ui_theme
+
+    db.commit()
+    db.refresh(user_prefs)
+    return user_prefs
+
 
 
 
